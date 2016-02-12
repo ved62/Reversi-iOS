8ef21a906c9e72798565baac865316677de598c4
ved62 <v.dembskiy@gmail.com>
2016-02-12 17:02:30 +0300

    Initial Commit

---
+++ Reversi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reversi
+//
+//  Created by Владислав Эдуардович Дембский on 12.02.16.
+//  Copyright © 2016 Vladislav Dembskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
